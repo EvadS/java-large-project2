503dc34d3ceeab409b882ce00a1026df17aa4e1f
Yevheniy Skyba <yevhen.skyba@ligazakon.ua>
2025-08-14 19:08:41 +0300

    forth commit

--- src/main/java/com/se/sample/App.java
+++ src/main/java/com/se/sample/App.java
@@ -5,6 +5,6 @@ package com.se.sample;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("third commit");
+        System.out.println("forth commit");
     }
 }
